ba94166d1f7a17c813ae70ef3a24e5243650a4f4
DonYau <DonYau@126.com>
2019-03-15 19:10:02 +0800

    增加广播秤测量SDK

--- QNSDK/QNBleDevice.h
+++ QNSDK/QNBleDevice.h
@@ -8,6 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSUInteger, QNDeviceType) {
+    QNDeviceTypeScaleBleDefault = 100, //普通蓝牙秤
+    QNDeviceTypeScaleBroadcastSingle = 120, //单向广播秤
+    QNDeviceTypeScaleBroadcastDouble = 121, //双向广播秤
+};
+
 @interface QNBleDevice : NSObject
 /** mac地址 */
 @property (nonatomic, readonly, strong) NSString *mac;
@@ -21,4 +27,7 @@
 @property (nonatomic, readonly, strong) NSNumber *RSSI;
 /** 是否已开机 */
 @property (nonatomic, readonly, getter=isScreenOn, assign) BOOL screenOn;
+/** 设备类型 */
+@property (nonatomic, assign) QNDeviceType deviceType;
+
 @end
