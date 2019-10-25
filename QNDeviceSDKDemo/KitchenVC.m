cb3a3844b84036c89da7e23a7a3814d8d1726d10
DonYau <DonYau@126.com>
2019-10-25 11:38:49 +0800

    修改demo

--- QNDeviceSDKDemo/KitchenVC.m
+++ QNDeviceSDKDemo/KitchenVC.m
@@ -10,8 +10,13 @@
 #import <QNSDK/QNDeviceSDK.h>
 
 @interface KitchenVC ()<QNBleDeviceDiscoveryListener>
-@property (weak, nonatomic) IBOutlet UILabel *peelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *macLabel;
+@property (weak, nonatomic) IBOutlet UILabel *modeLabel;
 @property (weak, nonatomic) IBOutlet UILabel *weightLabel;
+@property (weak, nonatomic) IBOutlet UILabel *peelLabel;
+@property (weak, nonatomic) IBOutlet UILabel *overloadLabel;
+@property (weak, nonatomic) IBOutlet UILabel *negativeLabel;
+
 
 @end
 
@@ -19,7 +24,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.peelLabel.hidden = YES;
     [QNBleApi sharedBleApi].discoveryListener = self;
 }
 
@@ -38,37 +42,38 @@
 }
 
 - (void)onKitchenDeviceDiscover:(QNBleKitchenDevice *)device {
-    self.peelLabel.hidden = !device.isPeel;
-    if (device.isOverload) {
-        self.weightLabel.text = @"Err";
-    } else {
-        NSString *weightStr = nil;
-        switch (device.unit) {
-            case QNUnitML:
-                weightStr = [NSString stringWithFormat:@"%.1f ml",[[QNBleApi sharedBleApi] convertWeightWithTargetUnit:device.weight unit:QNUnitML]];
-                break;
+    self.macLabel.text = device.mac;
+    self.modeLabel.text = device.modeId;
+    self.peelLabel.text = device.isPeel ? @"true" : @"false";
+    self.overloadLabel.text = device.isOverload ? @"true" : @"false";
+    self.negativeLabel.text = device.isNegative ? @"true" : @"false";
+
+    NSString *weightStr = nil;
+    switch (device.unit) {
+        case QNUnitML:
+            weightStr = [NSString stringWithFormat:@"%.1f ml",[[QNBleApi sharedBleApi] convertWeightWithTargetUnit:device.weight unit:QNUnitML]];
+            break;
             
-            case QNUnitOZ:
-                weightStr = [NSString stringWithFormat:@"%.1f ml",[[QNBleApi sharedBleApi] convertWeightWithTargetUnit:device.weight unit:QNUnitOZ]];
-                break;
-            case QNUnitLBOZ:
-            {
-                double ozNum = [[QNBleApi sharedBleApi] convertWeightWithTargetUnit:device.weight unit:QNUnitOZ];
-                int lbNum = ozNum / 16;
-                ozNum = ozNum - lbNum * 16;
-                weightStr = [NSString stringWithFormat:@"%d lb %.1f oz",lbNum,ozNum];
-            }
-                break;
-            default:
-                weightStr = [NSString stringWithFormat:@"%.1f g",device.weight];
-                break;
+        case QNUnitOZ:
+            weightStr = [NSString stringWithFormat:@"%.1f ml",[[QNBleApi sharedBleApi] convertWeightWithTargetUnit:device.weight unit:QNUnitOZ]];
+            break;
+        case QNUnitLBOZ:
+        {
+            double ozNum = [[QNBleApi sharedBleApi] convertWeightWithTargetUnit:device.weight unit:QNUnitOZ];
+            int lbNum = ozNum / 16;
+            ozNum = ozNum - lbNum * 16;
+            weightStr = [NSString stringWithFormat:@"%d lb %.1f oz",lbNum,ozNum];
         }
-        
-        if (device.isNegative) {
-            weightStr = [@"- " stringByAppendingString:weightStr];
-        }
-        self.weightLabel.text = weightStr;
+            break;
+        default:
+            weightStr = [NSString stringWithFormat:@"%.1f g",device.weight];
+            break;
     }
+    
+    if (device.isNegative) {
+        weightStr = [@"- " stringByAppendingString:weightStr];
+    }
+    self.weightLabel.text = weightStr;
 }
 
 
